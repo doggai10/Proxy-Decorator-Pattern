dffe637514bd84b4297717d38764c680e7198af4
doggai10 <doggai77@gmail.com>
2022-03-04 00:17:37 +0900

    add proxy pattern ex

---
+++ src/test/java/hello/proxy/pureproxy/ProxyPatternTest.java
@@ -0,0 +1,30 @@
+package hello.proxy.pureproxy;
+
+import hello.proxy.pureproxy.code.CacheProxy;
+import hello.proxy.pureproxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.rowset.CachedRowSet;
+
+public class ProxyPatternTest {
+
+    @Test
+    public void noProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    public void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
